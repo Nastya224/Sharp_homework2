2683fa81e36e93b2a88bae4b587a2ae530f77e46
Nastya_keks <aagolunova@edu.hse.ru>
2022-09-26 12:38:27 +0300

    task2

---
+++ task2-2/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Write("Введите число:");
+int N = int.Parse(Console.ReadLine());
+
+if (N < 1000000)
+{
+    if (N >= 100000)
+{
+    int third = N / 1000 % 10;
+    Console.WriteLine(third);
+}
+}
+
+if (N < 100000)
+{
+    if (N >= 10000)
+{
+    int third = N / 100 % 10;
+    Console.WriteLine(third);
+}
+}
+
+if (N < 10000)
+{
+    if (N >= 1000)
+{
+    int third = N /10 % 10;
+    Console.WriteLine(third);
+}
+}
+
+if (N < 1000)
+{
+    if (N >= 100)
+{
+    int third = N % 10;
+    Console.WriteLine(third);
+}
+}
+
+if (N < 100) 
+{
+Console.WriteLine("третьей цифры нет");
+}
+
+
